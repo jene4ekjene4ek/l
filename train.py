6e145809f3345b7137de1a50c1117b3e57b820aa
jene4ekjene4ek <32958504+jene4ekjene4ek@users.noreply.github.com>
2019-10-15 10:30:20 +0300

    Update train.py

--- train.py
+++ train.py
@@ -19,60 +19,60 @@ import mlflow
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--max_depth", dest="max_depth", help="max_depth", default=2, type=int)
-    parser.add_argument("--max_bins", dest="max_bins", help="max_bins", default=32, type=int)
-    args = parser.parse_args()
-    current_file = os.path.basename(__file__)
+    #parser = ArgumentParser()
+    #parser.add_argument("--max_depth", dest="max_depth", help="max_depth", default=2, type=int)
+    #parser.add_argument("--max_bins", dest="max_bins", help="max_bins", default=32, type=int)
+    #args = parser.parse_args()
+    #current_file = os.path.basename(__file__)
 
     with mlflow.start_run():
-        mlflow.log_param("max_depth", args.max_depth)
-        mlflow.log_param("max_bins", args.max_bins)
-        print("Parameters: max_depth: {}  max_bins: {}".format(max_depth,max_bins))
-    spark = SparkSession.builder.appName("DecisionTreeClassificationExample").getOrCreate()
+        #mlflow.log_param("max_depth", args.max_depth)
+        #mlflow.log_param("max_bins", args.max_bins)
+        #print("Parameters: max_depth: {}  max_bins: {}".format(max_depth,max_bins))
+        spark = SparkSession.builder.appName("DecisionTreeClassificationExample").getOrCreate()
 
     # Load the data stored in LIBSVM format as a DataFrame.
-    data_path = "./sample_libsvm_data.txt"
-    data = spark.read.format("libsvm").load(data_path)
+        data_path = "./sample_libsvm_data.txt"
+        data = spark.read.format("libsvm").load(data_path)
 
     # Index labels, adding metadata to the label column.
     # Fit on whole dataset to include all labels in index.
-    label_indexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(data)
+        label_indexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(data)
 
     # Automatically identify categorical features, and index them.
     # We specify maxCategories so features with > 4 distinct values are treated as continuous.
-    feature_indexer = VectorIndexer(
-        inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
+        feature_indexer = VectorIndexer(
+            inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
 
     # Split the data into training and test sets
-    (trainingData, testData) = data.randomSplit([0.7, 0.3])
+        (trainingData, testData) = data.randomSplit([0.7, 0.3])
 
     # Train a DecisionTree model.
-    dt = DecisionTreeClassifier(labelCol="indexedLabel",
+        dt = DecisionTreeClassifier(labelCol="indexedLabel",
                                 featuresCol="indexedFeatures",
                                 maxDepth=max_depth,
                                 maxBins=max_bins)
 
     # Chain indexers and tree in a Pipeline.
-    pipeline = Pipeline(stages=[label_indexer, feature_indexer, dt])
+        pipeline = Pipeline(stages=[label_indexer, feature_indexer, dt])
 
     # Train model.  This also runs the indexers.
-    model = pipeline.fit(trainingData)
+        model = pipeline.fit(trainingData)
 
     # Make predictions
-    predictions = model.transform(testData)
+        predictions = model.transform(testData)
 
     # Select example rows to display.
-    predictions.select("prediction", "indexedLabel", "features").show(5)
+        predictions.select("prediction", "indexedLabel", "features").show(5)
 
     # Select (prediction, true label) and compute test error.
-    evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel",
+        evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel",
                                                   predictionCol="prediction",
                                                   metricName="accuracy")
-    accuracy = evaluator.evaluate(predictions)
-    test_error = 1.0 - accuracy
-    print("Test Error = {} ".format(test_error))
+        accuracy = evaluator.evaluate(predictions)
+        test_error = 1.0 - accuracy
+        print("Test Error = {} ".format(test_error))
 
-    tree_model = model.stages[2]
-    print(tree_model)
-    spark.stop()
+        tree_model = model.stages[2]
+        print(tree_model)
+        spark.stop()
