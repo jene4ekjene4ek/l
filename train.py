3769b35eb37f42bbf052c324cfdbdfa4c5d6053f
jene4ekjene4ek <32958504+jene4ekjene4ek@users.noreply.github.com>
2019-10-23 12:40:11 +0300

    Update train.py

--- train.py
+++ train.py
@@ -27,57 +27,57 @@ def train(data_path, max_depth, max_bins):
     data = spark.read.format("libsvm").load(data_path)
     
     df_l = data.select('label')
-    df_l.repartition(1).write.csv('data.csv', mode='overwrite', sep='\t', header=True)
+    df_l.repartition(1).write.csv('data', mode='overwrite', sep='\t', header=True)
 
-    # Index labels, adding metadata to the label column.
-    # Fit on whole dataset to include all labels in index.
-    label_indexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(data)
+#     # Index labels, adding metadata to the label column.
+#     # Fit on whole dataset to include all labels in index.
+#     label_indexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(data)
 
-    # Automatically identify categorical features, and index them.
-    # We specify maxCategories so features with > 4 distinct values are treated as continuous.
-    feature_indexer = VectorIndexer(
-        inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
+#     # Automatically identify categorical features, and index them.
+#     # We specify maxCategories so features with > 4 distinct values are treated as continuous.
+#     feature_indexer = VectorIndexer(
+#         inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
 
-    # Split the data into training and test sets
-    (trainingData, testData) = data.randomSplit([0.7, 0.3])
+#     # Split the data into training and test sets
+#     (trainingData, testData) = data.randomSplit([0.7, 0.3])
 
-    # Train a DecisionTree model.
-    dt = DecisionTreeClassifier(labelCol="indexedLabel",
-                                featuresCol="indexedFeatures",
-                                maxDepth=max_depth,
-                                maxBins=max_bins)
+#     # Train a DecisionTree model.
+#     dt = DecisionTreeClassifier(labelCol="indexedLabel",
+#                                 featuresCol="indexedFeatures",
+#                                 maxDepth=max_depth,
+#                                 maxBins=max_bins)
 
-    # Chain indexers and tree in a Pipeline.
-    pipeline = Pipeline(stages=[label_indexer, feature_indexer, dt])
+#     # Chain indexers and tree in a Pipeline.
+#     pipeline = Pipeline(stages=[label_indexer, feature_indexer, dt])
 
-    # Train model.  This also runs the indexers.
-    model = pipeline.fit(trainingData)
+#     # Train model.  This also runs the indexers.
+#     model = pipeline.fit(trainingData)
 
-    # Make predictions
-    predictions = model.transform(testData)
+#     # Make predictions
+#     predictions = model.transform(testData)
 
-    # Select example rows to display.
-    predictions.select("prediction", "indexedLabel", "features").show(5)
+#     # Select example rows to display.
+#     predictions.select("prediction", "indexedLabel", "features").show(5)
 
-    # Select (prediction, true label) and compute test error.
-    evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel",
-                                                  predictionCol="prediction",
-                                                  metricName="accuracy")
-    accuracy = evaluator.evaluate(predictions)
-    test_error = 1.0 - accuracy
-    print("Test Error = {} ".format(test_error))
+#     # Select (prediction, true label) and compute test error.
+#     evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel",
+#                                                   predictionCol="prediction",
+#                                                   metricName="accuracy")
+#     accuracy = evaluator.evaluate(predictions)
+#     test_error = 1.0 - accuracy
+#     print("Test Error = {} ".format(test_error))
 
-    tree_model = model.stages[2]
-    print(tree_model)
+#     tree_model = model.stages[2]
+#     print(tree_model)
     
-    mlflow.log_param('max_depth', max_depth)
-    mlflow.log_param('max_bins', max_bins)
+#     mlflow.log_param('max_depth', max_depth)
+#     mlflow.log_param('max_bins', max_bins)
     
-    mlflow.spark.log_model(model, '')
-    mlflow.spark.save_model(model, '')
-#     try:
-#     mlflow.spark.save_model(model, save_path)
-#     except: 
+#     mlflow.spark.log_model(model, '')
+#     mlflow.spark.save_model(model, '')
+# #     try:
+# #     mlflow.spark.save_model(model, save_path)
+# #     except: 
     
     spark.stop()
     
